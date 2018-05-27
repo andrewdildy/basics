75f42bf2ca6e61f69b464886426cc4929c5cc981
andrew <afd131@gmail.com>
2018-05-26 17:24:12 -0700

    clean up

--- datastructures/hashtable/hashtable.py
+++ datastructures/hashtable/hashtable.py
@@ -1,13 +1,4 @@
-class Entry:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-    def __eq__(self, other):
-        if type(other) == Entry:
-            return self.key == other.key
-        if type(other) == type(self.key):
-            return self.key == other
-        return False
+from entry import Entry
 
 class HashTable:
 
